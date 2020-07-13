10697c06bcfaa86c904a56ba47452e79fb7c5117
WILLIAMS Olatubi <wolatub@gmail.com>
2020-07-13 16:01:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+print("Williams")
+number_of_doughnuts = 5
+number_of_doughnuts ="f"
+number_of_doughnuts = True
+number_of_doughnuts = 5.5
+print(number_of_doughnuts)
+doughnut_name = "Glaze"
+name = "Hello"
+doughnut_name = "name"
+
+print(doughnut_name)
+
+Doughnut_exist = True
+amount_of_food = 10
+print(amount_of_food + number_of_doughnuts)
+
+amount_of_food -= 1
+amount_of_food = amount_of_food - 1
+if number_of_doughnuts == 0:
+ Doughnut_exist = False
+elif number_of_doughnuts >=5:
+  number_of_doughnuts -=1
+else:
+  number_of_doughnuts += 1
+
+Life = 50
+if Life == 0:
+  print("you have died")
+else:
+  Life -= 1
+print(Life)
+
+
+print("create a username")
+username = input("what is yourname:\n ")
+age = input("Hello " + username+", how old are you?\n")
+if int(age) >= 13:
+  print(username + "welcome to the site" )
+elif int(age) <=12:
+  print(username +" you are not old enough to view this site")
+age = 15
+if age >= 100:
+  print("account invalid")
+elif age >= 13: # 100 > age >=13
+  print("account active")
+else:
+  print("too young")
+
+#true if both are >10
+if not age >= 10 or age2 >= 10
+print("example")  
+
