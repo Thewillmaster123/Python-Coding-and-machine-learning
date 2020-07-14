314e99e525f650a70cf1b0aace9214fb87790d4d
WILLIAMS Olatubi <wolatub@gmail.com>
2020-07-14 14:11:02 +0000

    day 1 code

--- main.py
+++ main.py
@@ -34,16 +34,16 @@ print(Life)
 print("create a username")
 username = input("what is yourname:\n ")
 age = input("Hello " + username+", how old are you?\n" )
-if int(age) >=13:
+if int(age)>12 and int(age)<99:
   print(username + "welcome to the site" )
 elif int(age) <=12:
   print(username + "you are not old enough to view this site" )
-elif int(age) >=99:
+else:
   print(username + "how are you even operating the computer" )
 
-if int(age) >= 100:
+if int(age) >= 99:
   print("account invalid")
-elif age >= 13: # 100 > age >=13
+elif int(age) >= 13: # 100 > age >=13
   print("account active")
 else:
   print("too young")
@@ -51,9 +51,63 @@ else:
 
  
 
-a=6
-b=3
+a=3
+b=6
 if a>b:
   print("a is greater than b")
 
+x = 2 
+print(x) 
+if x > 3:
+  x += 2
+print(x)
 
+has_key = True 
+if has_key == True:
+  print("You won! Unlock the door!") 
+
+player_age = 22
+
+if player_age >= 21:
+  print("You could be a teacher")
+elif player_age >= 18:
+  print("You could be in college.")
+elif player_age >= 13:
+  print("You can also attend iD Academies!")
+elif player_age >= 7:
+  print("You can attend iD Tech Camps!")
+else:
+  print("You're young.")
+
+player_has_item = True
+score = 150
+won = False
+
+if player_has_item and score > 100:
+    won = True
+
+if not won:
+    print("You haven't beaten the game yet.")
+elif won:
+    print("You won the game!")
+
+
+player_has_item = False
+score = 202
+won = False
+
+if player_has_item or score > 200:
+    won = True
+
+if not won:
+    print("You haven't beaten the game yet.")
+elif won:
+    print("You won the game!")
+
+
+
+x = 0
+
+while x <= 10:
+  print(x)
+  x += 1
