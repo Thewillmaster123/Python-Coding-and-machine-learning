3bbbabb6cb53b4d0325822ad9c53fc9133c92857
WILLIAMS Olatubi <wolatub@gmail.com>
2020-07-17 14:04:14 +0000

    day 4 code

--- main.py
+++ main.py
@@ -1,57 +1,23 @@
-class MyPet:
-  def __init__(self,pet_species, pet_name, pet_age):
-    self.species =pet_species
-    self.name = pet_name
-    self.age = pet_age
+import numpy as np 
 
-  
+numbers = np.array([[1,2],[3,4]])
+print(numbers)
+print(numbers.shape)
 
+array2 = np.array([[1,2,3],[4,5,6]])
+print(array2)
+print(array2[0,2])
 
+num1 = [1,1,1,1,1]
+num2 = [1,1,1,1,1]
+print(num1 + num2)
 
-class Alien:
-  def __init__(self,Alien_species, Alien_name, num_arms):
-    self.species =Alien_species
-    self.name = Alien_name
-    self.arms = int(num_arms)
+for x in range(len(num1)):
+  num1[x] = num1[x] + num2[x]
+num1.append(3)
+print(num1)
 
-  def high_five(self):
-    for x in range(0,self.arms):
-      print(self.name + "gives you a high five")
+arr1 = np.array([1,2,3,4,5])
+arr2 = np.array([6,7,8,9,10])
+print(arr1 + 5)
 
-Blue_Alien = Alien("Green_Alien","josh", 2 )
-print(Blue_Alien.name)
-Blue_Alien.high_five()
-
-class Human(Alien):
-  def __init__(self,human_name,region_lived_in):
-    self.name =human_name
-    self.species = "Human"
-    self.arms = 2
-    self.region = region_lived_in
-
-  def talk(self,speech):
-    print(speech)
-
-Jeff = Human("jeff","united States")
-Jeff.talk("Hello,I'm Jeff")
-
-class Dog(MyPet):
-  def __init__(self, dog_age, dog_name):
-    self.species = "dog"
-    self.age = dog_age
-    self.name = dog_name
-
-
-  def play_fetch(self):
-   print("Your pet" + self.name + "seems happy to play fetch")
-
-Ralph = Dog(2,"Ralph")
-Ralph.play_fetch()
-
-
-
-
-
-
-
-    
